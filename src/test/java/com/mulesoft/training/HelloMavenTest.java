4ef4f8e8523b39fe201206d70da733eaf3fc10a8
Ankur Agrawal <Ankur.Agrawal@mphasis.com>
2018-02-27 11:10:52 -0800

    Added global XML and formatted all xml files

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -25,7 +25,8 @@ public class HelloMavenTest extends FunctionalTestCase {
     }
     
     @Override
-    protected String getConfigFile() {
-        return "maven-project.xml";
+    protected String[] getConfigFiles() {
+        String[] files = {"maven-project.xml", "global.xml"};
+        return files;
     }
 }
